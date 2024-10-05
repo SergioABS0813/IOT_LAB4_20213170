bc61690ec22a13a71a743f00497cd3a59290c8c2
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-10-05 00:27:51 -0500

    con animacion entre fragments

--- app/src/main/java/com/example/a20213170_lab4/AppActivity.java
+++ app/src/main/java/com/example/a20213170_lab4/AppActivity.java
@@ -1,14 +1,29 @@
 package com.example.a20213170_lab4;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AppActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+    private int currentFragmentIndex = 0; // Marcador de posición actual
+    private LigasFragment ligasFragment = new LigasFragment();
+    private PosicionesFragment posicionesFragment = new PosicionesFragment();
+    private ResultadosFragment resultadosFragment = new ResultadosFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +35,78 @@ public class AppActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView = findViewById(R.id.bottomView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, ligasFragment)
+                    .commit();
+        }
+
+        //logica de bottomNav
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if(itemId == R.id.navLigas){
+                    if (currentFragmentIndex != 0) {  // Solo reemplazar si no está en Inicio
+                        getSupportFragmentManager().beginTransaction()
+                                .setCustomAnimations(R.anim.swipe_in_right, R.anim.swipe_out_right)
+                                .replace(R.id.frameLayout, ligasFragment)
+                                .addToBackStack(null)  // Agregar a la pila de retroceso
+                                .commit();
+                        currentFragmentIndex = 0;  // Actualizar el índice actual del fragmento
+                    }
+                    return true;
+                } else if (itemId == R.id.navPosiciones) {
+                    if (currentFragmentIndex == 0) {  // Solo reemplazar si no está en Inicio
+                        getSupportFragmentManager().beginTransaction()
+                                .setCustomAnimations(R.anim.swipe_in_left, R.anim.swipe_out_left)
+                                .replace(R.id.frameLayout, posicionesFragment)
+                                .addToBackStack(null)  // Agregar a la pila de retroceso
+                                .commit();
+                        currentFragmentIndex = 1;  // Actualizar el índice actual del fragmento
+                    } else if (currentFragmentIndex == 2) {
+                        getSupportFragmentManager().beginTransaction()
+                                .setCustomAnimations(R.anim.swipe_in_right, R.anim.swipe_out_right)
+                                .replace(R.id.frameLayout, posicionesFragment)
+                                .addToBackStack(null)  // Agregar a la pila de retroceso
+                                .commit();
+                        currentFragmentIndex = 1;  // Actualizar el índice actual del fragmento
+
+                    }
+                    return true;
+                } else if (itemId == R.id.navResultados) {
+                    if (currentFragmentIndex != 2) {  // Solo reemplazar si no está en Inicio
+                        getSupportFragmentManager().beginTransaction()
+                                .setCustomAnimations(R.anim.swipe_in_left, R.anim.swipe_out_left)
+                                .replace(R.id.frameLayout, resultadosFragment)
+                                .addToBackStack(null)  // Agregar a la pila de retroceso
+                                .commit();
+                        currentFragmentIndex = 2;  // Actualizar el índice actual del fragmento
+                    }
+                    return true;
+                }
+                cargarFragmento(new LigasFragment(), true);
+                return true;
+            }
+        });
+
+    }
+
+    private void cargarFragmento (Fragment fragment, boolean isAppInitialized){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if(isAppInitialized){
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        }else{
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.commit();
+
     }
 
 }
--- app/src/main/java/com/example/a20213170_lab4/MainActivity.java
+++ app/src/main/java/com/example/a20213170_lab4/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.a20213170_lab4;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -28,15 +34,47 @@ public class MainActivity extends AppCompatActivity {
 
         btnIngresar = findViewById(R.id.btnIngresar);
 
-        btnIngresar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AppActivity.class);
-                startActivity(intent);
-            }
-        });
+        // Validar conexión a Internet
+        if (!isConnectedToInternet()) {
+            showInternetDialog();
+            btnIngresar.setEnabled(false);
+        }
 
+        if(isConnectedToInternet()){
+            btnIngresar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, AppActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                }
+            });
+        }
 
+    }
 
+    private boolean isConnectedToInternet() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnected();
+    }
+
+    // Mostrar diálogo si no hay conexión a Internet
+    private void showInternetDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Conexión a Internet requerida")
+                .setMessage("No hay conexión a Internet. Por favor, conecte su dispositivo a una red.")
+                .setPositiveButton("Configuración", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Abrir la configuración de Wi-Fi del dispositivo
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
